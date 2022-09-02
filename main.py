5f3b99350da47ccb8be3654591577d576dde811f
Joseph N. Rohani <britishperson10>
2022-09-02 15:31:40 -0700

    update

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 #!/bin/python
 # Copyright (C) Joseph Rohani
-import sys, urllib.request, os, shutil
-
+import sys, urllib.request, os, shutil, time
+global DEBUG
+if "-d" in sys.argv:
+    DEBUG=True
+else:
+    DEBUG=False
 class Flags:
-    def choose_flag(flag):
-        pass
+    def choose_flag(argc, argv):
+        for x in argv:
+            if x=="-h" or x=="--help":
+                print("")
 
 def check_dir(path="data"):
     if not os.path.exists((path+"/")):
@@ -12,34 +18,60 @@ def check_dir(path="data"):
 
 def download(name, ow=False, path="data"):
     global location, file_location
+    name_orig=name
+    name=name.lower()
     location=os.path.dirname(os.path.realpath(__file__))+"/data/"+name+"/"
     file_location=location+name
     if os.path.exists(location):
-        answer=input("The path exists, would you like to overwrite the path[y/N]:  ")
-        if answer=="y" or answer=="Y":
+        
+        if DEBUG:  pass
+        elif input("The path exists, would you like to overwrite the path[y/N]:  ").lower()=="y":
             shutil.rmtree(f"{path}/{name}")
             os.mkdir(f"data/{name}")
+            url=f"https://mcassessor.maricopa.gov/mcs/export/property/?q={name}"
+            urllib.request.urlretrieve(url, f"{path}/{name}/{name}")
+            print(f"File saved to:  {location}")
+        elif answer.lower()=="r":
+            pass
         else:
             print("Closing program")
             exit(0)
-    elif not os.path.exists(location):
+    else:
         os.mkdir(f"data/{name}")
-    url=f"https://mcassessor.maricopa.gov/mcs/export/property/?q={name}"
-    urllib.request.urlretrieve(url, f"{path}/{name}/{name}")
-    print(f"File saved to:  {location}")
+        url=f"https://mcassessor.maricopa.gov/mcs/export/property/?q={name}"
+        urllib.request.urlretrieve(url, f"{path}/{name}/{name}")
+        print(f"File saved to:  {location}")
+
+    
 
 def sift(data):
     print(location)
     file=open(file_location, "r")
     line_list=[]
     for (i, line) in enumerate(file):
-        if data in line:
+        line_upper=line.upper()
+        if data in line_upper:
             line_list.append(i)
+    file_contents=file.readlines()
     print(line_list)
+    print(file_contents)
+    print(file)
+    for x in line_list:
+        print(x)
+        print(file_contents[x])
+    
+    
+
 
 check_dir()
-name=sys.argv[1]
+try:
+    name=sys.argv[1]
+except:
+    name=input("Please enter a name:  ")
+start=time.time()
 download(name) #Add sys flags
-
-sift(name.upper())
+print(time.time()-start)
+start=time.time()
+sift(input("data: ").upper())
+print(time.time()-start)
 
